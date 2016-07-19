f172ec78601eafe28f47b1db0b267fa4a1cd88c3
michael ozeri <ozeri.mich@gmail.com>
2016-07-19 16:35:13 +0300

    config constructor  - michael
    half of constructor function built

--- SPConfig.c
+++ SPConfig.c
@@ -2,7 +2,7 @@
  * SPConfig.c
  *
  *  Created on: 19 αιεμι 2016
- *      Author: michael
+ *      Author: michael ozeri & yuval ailer
  */
 
 #include <stdio.h>
@@ -34,10 +34,52 @@ struct sp_config_t{
 
 
 SPConfig spConfigCreate(const char* filename, SP_CONFIG_MSG* msg){
+
 	assert(msg != NULL);
+	FILE* fp = fopen(filename,"r");
+	char temp;
+	char* field;
+	int fk = 0; //field current fill pointer
+	char* value;
+	int vk = 0; //value current fill pointer
+	int mode = 0; // awaiting read mode for next char, new line(0), fieldname(1) , value(3), =(2),comment(4)
+	SPConfig config = (SPConfig)malloc(sizeof(SPConfig));
+	bool end = true;
+	while((temp=fgetc(fp))!= EOF){
+		if (mode == 4){ // reading a comment from file
+			if (temp != "\n"){
+				continue;
+			}else{
+				fk = 0;
+				vk = 0;
+				mode = 0;
+			}
+		}
+		else if (mode == 0){
+			if (temp == '#'){
+				mode = 4;
+				continue;
+			}else if (temp == ' '){
+				continue;
+			}else if (temp != "\n"){
+				*(field + fk) = temp;
+				fk++;
+				mode = 1;
+			}else{
+				continue;
+			}
+
+		}else if (mode==1){
 
 
+		}else if(mode==2){
 
+		}else{
+
+		}
+	}
+	fclose(fp);
+	return config;
 }
 
 bool spConfigIsExtractionMode(const SPConfig config, SP_CONFIG_MSG* msg){
