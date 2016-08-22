ae59d93f374db3f4e11eebeb84bcf70077f06cf9
yuvalailer <yuvalailer@gmail.com>
2016-08-22 21:23:13 +0300

    started making the KDTree

--- KDTree.c
+++ KDTree.c
@@ -4,11 +4,29 @@
  *  Created on: 18 баев„ 2016
  *      Author: yuval
  */
-#include <stdlib.h>
-#include "SPConfig.h"
+
 #include "KDTree.h"
-#include <assert.h>
-#include <ctype.h>
+#include "SPKDArray.h"
+#include "SPPoint.h"
+
+// making a Tree Node struct
+
+typedef struct spTreeNode spTreeNode;
+struct spTreeNode {
+	int Dim; // The splitting dimension
+	int Val; // The median value of the splitting dimension
+	spTreeNode * Left; // Pointer to the left subtree
+	spTreeNode * Right; // Pointer to the right subtree
+	SPPoint* Data; // Pointer to a point (only if the current node is a leaf) otherwise this field value is NULL
+};
+
+spKdTreeNode Init(SPPoint* arr, int size){
+	spKdTreeNode ans = (spKdTreeNode)malloc(sizeof(spKdTreeNode));
+	SPKDArray array = init(arr,size);
+
+}
+
+
 
 
 
--- KDTree.h
+++ KDTree.h
@@ -14,5 +14,26 @@
 #include "SPLogger.h"
 #include "SPConfig.h"
 
+/*A Kd-tree struct. used for sorting and matching the picturs to one
+ another.
+ has the next fileds:
+ 1- Dim = The splitting dimension
+ 2- Val = The median value of the splitting dimension
+ 3- Left = Pointer to the left subtree
+ 4- Right = Pointer to the right subtree
+ 5- Data = Pointer to a point (only if the current node is a leaf) otherwise this field value is NULL
+ To initialize the kd-tree takes o(d * nLog(n)) time.
+
+ based on the lecture on kd-trees.
+ */
+
+typedef struct spKdTreeNode spKdTreeNode;
+
 
 #endif /* SOFTPRO_KDTREE_H_ */
+
+
+
+
+
+
