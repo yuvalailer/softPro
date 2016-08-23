58ab9ff7ee03c4d75902cf9bea5ccf2a961a76c1
michael ozeri <ozeri.mich@gmail.com>
2016-08-23 11:33:11 +0300

    change spkdarray to sort accoarding to pdex

--- SPKDArray.c
+++ SPKDArray.c
@@ -16,6 +16,7 @@ struct sp_kdarray_t {
 typedef struct tuple{
 	SPPoint point;
 	int sortingindex;
+	int pdex; // index in pointsarray
 }Tuple;
 
 int tuplesort(const void * x,const void * y){
@@ -27,31 +28,40 @@ SPKDArray Init(SPPoint* arr, int size){
 	SPKDArray ans = (SPKDArray)malloc(sizeof(SPKDArray));
 	int i;
 	int j;
+
 	int dim = spPointGetDimension(arr[0]); // checking dimension of sppoints in arr
+
 	Tuple* temp = (Tuple*)malloc(sizeof(Tuple)*size);
 	for (i = 0; i < size; i++) { //initialize special array of tuples
 		temp[i].point = spPointCopy(arr[i]);
 		temp[i].sortingindex = 0;
+		temp[i].pdex = i;
 	}
+
 	ans->mat = (int**)malloc(sizeof(int*)*dim); //initialize matrix of indexes
 	for (i = 0; i < dim; ++i) {
 		ans->mat[i] = (int*)malloc(sizeof(int)*size);
 	}
+
+
 	for (i = 0; i < dim; ++i) { //fill matrix of indexes
 		for(j=0;j<size;j++){
 			temp[j].sortingindex = i;
 		}
 		qsort(temp,size,sizeof(Tuple),tuplesort); // sort the array
 		for(j=0;j<size;j++){
-			ans->mat[i][j] = spPointGetIndex(temp[j].point);
+			ans->mat[i][j] = (temp[j].pdex);
 		}
 	}
+
 	ans->rows = dim;
 	ans->col = size;
-	ans->pointsarr = (SPPoint*)malloc(sizeof(SPPoint)*size);
+
+	ans->pointsarr = (SPPoint*)malloc(sizeof(SPPoint)*size); // copying arr into pointsarr
 	for (i = 0; i < size; ++i) {
 		ans->pointsarr[i] = spPointCopy(arr[i]);
 	}
+
 	free (temp);
 	return ans;
 }
@@ -139,58 +149,61 @@ SPKDArray* Split(SPKDArray kdArr, int coor){
 		return ans;
 }
 
-// internal use tester.
-//int main(){
-//
-//	int size = 5;
-//	int dim = 2;
-//
-//	SPPoint* arr = (SPPoint*)malloc(sizeof(SPPoint)*size);
-//
-//	double a1[2]= {1,2};
-//	double b1[2] = {123,70};
-//	double c1[2] = {2,7};
-//	double d1[2] = {9,11};
-//	double e1[2]= {3,4};
-//
-//	arr[0] = (spPointCreate(a1,dim,0));
-//	arr[1] = (spPointCreate(b1,dim,1));
-//	arr[2] = (spPointCreate(c1,dim,2));
-//	arr[3] = (spPointCreate(d1,dim,3));
-//	arr[4] = (spPointCreate(e1,dim,4));
-//
-//	SPKDArray kdarr = Init(arr,size);
-//
-//	int i,j;
-//
-//	/*for (i = 0; i < kdarr->rows; i++) {
-//		for (j = 0; j < kdarr->col;j++) {
-//			printf("%d ",kdarr->mat[i][j]);
-//		}
-//		printf("\n");
-//	}*/
-//	SPKDArray* sparr = Split(kdarr,0);
-//	SPKDArray ail1 = sparr[0];
-//	SPKDArray ail2 = sparr[1];
-//
-//	for (i = 0; i < 2; ++i) {
-//		for (j = 0; j < 3; ++j) {
-//			printf("%d ",ail1->mat[i][j]);
-//		}
-//		printf("\n");
-//	}
-//
-//	for (i = 0; i < 2; ++i) {
-//			for (j = 0; j < 2; ++j) {
-//				printf("%d ",ail2->mat[i][j]);
-//			}
-//			printf("\n");
-//		}
-//
-//	return 0;
-//}
-//
-//
+
+/*
+int main(){
+
+	int size = 5;
+	int dim = 2;
+
+	SPPoint* arr = (SPPoint*)malloc(sizeof(SPPoint)*size);
+
+	double a1[2]= {1,2};
+	double b1[2] = {123,70};
+	double c1[2] = {2,7};
+	double d1[2] = {9,11};
+	double e1[2]= {3,4};
+
+	arr[0] = (spPointCreate(a1,dim,0));
+	arr[1] = (spPointCreate(b1,dim,1));
+	arr[2] = (spPointCreate(c1,dim,2));
+	arr[3] = (spPointCreate(d1,dim,3));
+	arr[4] = (spPointCreate(e1,dim,4));
+
+	SPKDArray kdarr = Init(arr,size);
+
+	int i,j;
+
+	for (i = 0; i < kdarr->rows; i++) {
+		for (j = 0; j < kdarr->col;j++) {
+			printf("%d ",kdarr->mat[i][j]);
+		}
+		printf("\n");
+	}
+
+	SPKDArray* sparr = Split(kdarr,0);
+	SPKDArray ail1 = sparr[0];
+	SPKDArray ail2 = sparr[1];
+
+	for (i = 0; i < 2; ++i) {
+		for (j = 0; j < 3; ++j) {
+			printf("%d ",ail1->mat[i][j]);
+		}
+		printf("\n");
+	}
+
+	for (i = 0; i < 2; ++i) {
+			for (j = 0; j < 2; ++j) {
+				printf("%d ",ail2->mat[i][j]);
+			}
+			printf("\n");
+		}
+
+	return 0;
+}
+*/
+
+
 
 
 
